13ff60cf49ee776c1bb2d1306ef0c40145c9b333
JonathanKeilback <78490227+JonathanKeilback@users.noreply.github.com>
2021-05-11 16:23:39 -0400

    Add files via upload

---
+++ jonathanObjectProject/MixerChannel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jonathanObjectProject;
+
+/**
+ * Object class for Mixer.java. Contains fader/volume levels for each mixer channel.
+ * @author Jonathan
+ */
+class MixerChannel {
+    String channelName;
+    int faderLevel = 0;
+    int aux1Level = 0;
+    int aux2Level = 0;
+    int aux3Level = 0;
+    int aux4Level = 0;
+    //The scope of this object class is limited to the Java package.
+    
+}//close class
